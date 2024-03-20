779e95156b5fa65ad897895b50312abce99db7dc
lhermoso <leonardo@elif.capital>
2024-03-20 10:29:51 -0400

    Add .env-example and main.py for cryptocurrency analysis
    This commit adds two new files: .env-example and main.py. The .env-example file holds configurations for required APIs, while main.py introduces new classes for cryptocurrency analysis tasks. It includes research, financial, blockchain, and investment recommendations tasks, using agent roles for financial analysis, research, and investment advising.

---
+++ main.py
@@ -0,0 +1,57 @@
+from crewai import Crew
+from textwrap import dedent
+
+from crypto_tasks import CryptocurrencyAnalysisTasks
+from crypto_agents import CryptocurrencyAnalysisAgents
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class CryptoCrew:
+    def __init__(self, crypto):
+        agents = CryptocurrencyAnalysisAgents()
+
+        self.crypto = crypto
+        self.crypto_financial_analyst = agents.crypto_financial_analyst()
+        self.crypto_research_analyst = agents.crypto_research_analyst()
+        self.crypto_investment_advisor = agents.crypto_investment_advisor()
+
+    def run(self):
+        tasks = CryptocurrencyAnalysisTasks()
+        research_task = tasks.market_research(self.crypto_financial_analyst, self.crypto)
+        financial_task = tasks.tokenomics_analysis(self.crypto_research_analyst)
+        blockchain_task = tasks.blockchain_analysis(self.crypto_research_analyst)
+        recommend_task = tasks.investment_recommendation(self.crypto_investment_advisor)
+
+        crew = Crew(agents=[self.crypto_financial_analyst,
+                            self.crypto_research_analyst,
+                            self.crypto_investment_advisor
+
+                            ],
+                    tasks=[research_task,
+                           financial_task,
+                           blockchain_task,
+                           recommend_task],
+                    verbose=True
+                    )
+
+        result = crew.kickoff()
+        return result
+
+
+if __name__ == "__main__":
+    print("## Bem-vindo à Equipe de Análise Cripto IA")
+    print('-------------------------------')
+    company = input(
+        dedent("""
+           Qual é a criptomoeda que você deseja analisar?
+        """))
+
+    cripto_crew = CryptoCrew(company)
+    result = cripto_crew.run()
+    print("\n\n########################")
+    print("## Aqui está o Relatório")
+    print("########################\n")
+    print(result)
